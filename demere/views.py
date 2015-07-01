adf3f160b4bf45fc401c2545f3f6795da50ed063
Aashish Karki <0133asis@gmail.com>
2015-07-01 16:13:13 -0400

    Parses excel file correctly. Unicode problem though

--- demere/views.py
+++ demere/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, url_for, redirect
+from flask import render_template, request, url_for, redirect, jsonify
+from werkzeug import secure_filename
 from demere import demere
 from forms import upload_excel
 import json 
@@ -14,16 +15,23 @@ def upload():
             return render_template('index.html', form=form)
         else:      
             file = request.files[form.file.name]                  
+            json_data = read_excel(file)  
             
-            print "hello"
-            return render_template('list.html', form=form)
+            return render_template('list.html', form=form, data=json_data)
     elif request.method == 'GET':
         return render_template('index.html', form=form)    
 
 @demere.route('/uploads' , methods=['GET','POST'])
 def list_uploads():
     return render_template('list.html')
-def read_excel(filename):
-    list  = pe.get_array(file_name= filename)
-    json_string = json.dumps(list)
-    return json_string
+
+def read_excel(file):
+    """ Takes file and returns json """
+    if file: 
+        filename = file.filename
+        extension = filename.split(".")[1]
+        sheet = pe.get_sheet(file_type=extension, file_stream=file.read())
+        data  = pe.to_dict(sheet)            
+        for key in data:
+            print key, data[key]
+        return jsonify(data)
