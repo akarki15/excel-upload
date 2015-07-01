361dc5231fb68e8fdc6fddde05eb1965137ee02b
Aashish Karki <0133asis@gmail.com>
2015-07-01 15:11:36 -0400

    File uploads

--- demere/forms.py
+++ demere/forms.py
@@ -1,11 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms.fields import TextField, BooleanField, SubmitField,PasswordField
+from wtforms.fields import TextField, SubmitField,PasswordField
 from flask.ext.wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.validators import Required
 
 class upload_excel(Form):
 	
-	file = FileField('Excel File', validators = [FileAllowed(['xls', 'xlsx'], 'File format not supported!')])		
+	file = FileField('Excel File', validators = [FileAllowed(['xls', 'xlsx'], 'File format not supported!'), Required("Upload some file!")])		
 	submit = SubmitField("Upload!")	
 
 	# Constructor calls the Form's default constructor
--- demere/views.py
+++ demere/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, redirect
 from demere import demere
 from forms import upload_excel
 import json 
@@ -6,25 +6,24 @@ import pyexcel as pe
 import pyexcel.ext.xls # import it to handle xls file
 
 @demere.route('/' , methods=['GET','POST'])
-def upload():	
-	form = upload_excel()	
-	
-	if request.method == 'POST':		
-		if form.validate() == False:			
-			return render_template('index.html', form=form)
-		else:						
-			return redirect(url_for('list_excel'))
-	elif request.method == 'GET':
-		return render_template('index.html', form=form)
+def upload():   
+    form = upload_excel()   
+    
+    if request.method == 'POST':        
+        if form.validate() == False:                        
+            return render_template('index.html', form=form)
+        else:      
+            file = request.files[form.file.name]                  
+            
+            print "hello"
+            return render_template('list.html', form=form)
+    elif request.method == 'GET':
+        return render_template('index.html', form=form)    
 
-
-@demere.route('/list')
-def list_excel():
-    # return render_template('list.html')
-    return read_excel('sample.xlsx')
-
-
+@demere.route('/uploads' , methods=['GET','POST'])
+def list_uploads():
+    return render_template('list.html')
 def read_excel(filename):
-	list  = pe.get_array(file_name= filename)
-	json_string = json.dumps(list)
-	return json_string
+    list  = pe.get_array(file_name= filename)
+    json_string = json.dumps(list)
+    return json_string
